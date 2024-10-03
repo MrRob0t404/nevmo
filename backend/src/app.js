3a254b8f13b1aa80054b8b98c30afb980db5eb3f
Simon Gaviria <simongaviria1@gmail.com>
2024-10-03 19:22:11 -0400

    include transaction routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,20 @@
+// src/app.js
+
 const express = require("express");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
 
 dotenv.config();
 
 const app = express();
-
-// Middleware to parse JSON
 app.use(express.json());
 
-// User routes
+// Define routes
 app.use("/api/users", userRoutes);
+app.use("/api/transactions", transactionRoutes);
 
-// Error handling
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
-});
-
-// Start server
+// Start the server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
