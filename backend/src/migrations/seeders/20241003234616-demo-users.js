aaecc78613773ae2f10f99b714491f1fec9c9ab6
Simon Gaviria <simongaviria1@gmail.com>
2024-10-04 02:14:08 -0400

    fix migrations file for hashing passwords automatically

--- backend/src/migrations/seeders/20241003234616-demo-users.js
+++ backend/src/migrations/seeders/20241003234616-demo-users.js
@@ -1,23 +1,33 @@
 "use strict";
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("users", [
+    // Drop all existing users
+    await queryInterface.bulkDelete("users", null, {
+      truncate: true,
+      cascade: true,
+      restartIdentity: true,
+    });
+
+    const hashedUsers = await Promise.all([
       {
         username: "user1",
-        password: "password1", // BIG NO NO we should hash this in prod ...
+        password: await bcrypt.hash("password1", 10),
         balance: 100.0,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         username: "user2",
-        password: "password2", // BIG NO NO we should hash this in prod ...
+        password: await bcrypt.hash("password2", 10),
         balance: 50.0,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
     ]);
+
+    await queryInterface.bulkInsert("users", hashedUsers);
   },
 
   down: async (queryInterface, Sequelize) => {
